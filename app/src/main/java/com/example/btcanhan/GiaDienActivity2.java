75d8a42d5e8c260eace3db47fac212f93effecc8
Tranhuynhvu <tranhuynhvu2k4@gmail.com>
2024-11-27 07:45:04 +0700

    first commit

---
+++ app/src/main/java/com/example/btcanhan/GiaDienActivity2.java
@@ -0,0 +1,48 @@
+package com.example.btcanhan;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class GiaDienActivity2 extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_gia_dien2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        bottomNavigationView = findViewById(R.id.bottomnavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if(item.getItemId() == R.id.Ferturedid){
+                selectedFragment = new FedturedFragment();
+            }
+            else if(item.getItemId() == R.id.feedid){
+                selectedFragment = new ProfileFragment();
+            }if(item.getItemId() == R.id.profile){
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
+    }
+}
